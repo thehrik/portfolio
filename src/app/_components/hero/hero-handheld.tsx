4918fe940d02c336a19d44714096a7e99c9915d0
thehrik <hrik05malakar@gmail.com>
2025-04-17 21:55:52 +0530

    minor

--- src/app/_components/hero/hero-handheld.tsx
+++ src/app/_components/hero/hero-handheld.tsx
@@ -2,7 +2,7 @@ export default function HeroHandheld() {
   return (
     <div className="md:width-[400] relative flex h-150 flex-col items-center justify-center gap-6 bg-gradient-to-t from-[#99ddff] to-[white] lg:w-full dark:from-[#00264d] dark:to-[black]">
       <div className="flex h-35 w-35 rounded-[15px] bg-white"></div>
-      <div className="flex h-35 w-70 flex-col text-center align-middle text-black dark:text-white">
+      <div className="flex h-35 w-80 flex-col text-center align-middle text-black dark:text-white">
         <span className="text-[30px] font-bold text-[#0059b3] dark:text-[#80bfff]">
           Subhajit Malakar
         </span>
